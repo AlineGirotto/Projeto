8b113cba41aa506c9ab505b71a381b64116e918c
Aline Girotto <alinegirotto07@gmail.com>
2021-08-24 19:02:48 -0300

    primeiro commit

---
+++ public/javascripts/index.js
@@ -0,0 +1,225 @@
+var form = document.getElementById('form');
+form.addEventListener('submit', verificar);
+
+var btVer = document.getElementById('val');
+btVer.addEventListener('click', verificar);
+
+var p = document.getElementById('msg');
+
+function verificar(e) {
+    var cont = 0;
+    if(verificaN() == true){
+        cont ++;
+    }
+    if(verificaD() == true){
+        cont ++;
+    }
+    if(verificaE() == true){
+        cont ++;
+    }
+    if(verificaCpf() == true){
+        cont ++;
+    }
+    if(verificaCep() == true){
+        cont ++;
+    }
+    if(verificaNum() == true){
+        cont ++;
+    }
+    if(verificaSexo() == true){
+        cont ++;
+    }
+    if(verificaTel() == true){
+        cont ++;
+    }
+
+    if(cont != 8){
+        e.preventDefault();
+    }
+}
+
+function verificaN() {
+    var nmc = document.getElementById('nome');
+    var nm = document.getElementById("nome").value;
+    var numeros = "";
+
+    for (var i = 0; i < nm.length; i++) {
+        if (nm.charAt(i) != " ") {
+            if (!isNaN(nm.charAt(i))) {
+                numeros += nm.charAt(i);
+            }
+        }
+    }
+
+    if (nm == '' || nm.trim().indexOf(' ') == -1 || nm.length < 6 || numeros.length > 0) {
+        nmc.setAttribute("class", "red");
+        return false;
+    } else {
+        nmc.setAttribute("class", "");
+        return true;
+    }
+
+}
+
+function verificaD() {
+    var dt = document.getElementById('date');
+    var xdt = "";
+    for (var i = 0; i <= dt.value.length; i++) {
+        if (dt.value.charAt(i) != "-") {
+            xdt += dt.value.charAt(i);
+        }
+    }
+    var data = new Date();
+    if (dt.value == '') {
+        dt.setAttribute("class", "red");
+        return false;
+    } else {
+        var ano = xdt.substring(0, 4);
+        if(ano.length<5){
+            var mes = xdt.substring(4, 6);
+            var dia = xdt.substring(6, 8);
+            var data_nasc = new Date(mes + '/' + dia + "/" + ano);
+
+            var anoa = data.getFullYear();
+            var mesa = data.getMonth();
+            var diaa = data.getDate();
+            var data_atual = new Date(mesa + '/' + diaa + "/" + anoa);
+
+            if (data_nasc >= data_atual) {
+                dt.setAttribute("class", "red");
+                return false;
+            } else {
+                dt.removeAttribute("class", "red");
+                return true;
+            }
+        }else{
+            dt.setAttribute("class","red");
+            return false;
+        }
+    }
+
+}
+
+function verificaE() {
+    var em = document.getElementById('email');
+    if (em.value.indexOf(' ') != -1 || (em.value.indexOf('@') == -1) || em.value.indexOf('@') != em.value.lastIndexOf('@') || em.value.indexOf('@') > em.value.lastIndexOf('.')) {
+        em.setAttribute("class", "red");
+        return false;
+    } else {
+        em.removeAttribute("class", "red");
+        return true;
+    }
+}
+
+function verificaCpf() {
+    var c = document.getElementById('cpf');
+    var xcpf = "";
+    var soma = 0;
+    var resto = 0;
+
+    if (c.value.length == 3 || c.value.length == 7) {
+        c.value += ".";
+    } else if (c.value.length == 11) {
+        c.value += "-";
+    }
+
+    for (var i = 0; i <= c.value.length; i++) {
+        if (c.value.charAt(i) != "." && c.value.charAt(i) != "-") {
+            xcpf += c.value.charAt(i);
+        }
+    }
+
+    if (c.value == '' || xcpf.length <= 10 || isNaN(xcpf)) {
+        c.setAttribute("class", "red");
+        return false;
+    } else {
+        if (xcpf == "00000000000" || xcpf == "11111111111" || xcpf == "22222222222" || xcpf == "33333333333" || xcpf == "44444444444" || xcpf == "55555555555" || xcpf == "66666666666" || xcpf == "77777777777" || xcpf == "88888888888" || xcpf == "99999999999") {
+            c.setAttribute("class","red");
+            return false;
+        }else{
+            c.removeAttribute("class","red");
+            return true;
+        }
+    }
+}
+
+
+function verificaCep() {
+    var cp = document.getElementById('cep');
+    var xcep = "";
+
+    if (cp.value.length == 5) {
+        cp.value += "-";
+    }
+    for (var i = 0; i <= cp.value.length; i++) {
+        if (cp.value.charAt(i) != "." && cp.value.charAt(i) != "-") {
+            xcep += cp.value.charAt(i);
+        }
+    }
+    if (cp.value == '' || xcep.length <= 7 || isNaN(xcep)) {
+        cp.setAttribute("class", "red");
+        return false;
+    } else {
+        cp.removeAttribute("class","red");
+        return true;
+    }
+}
+
+function verificaNum() {
+    var n = document.getElementById("num");
+    if (n.value == '' || isNaN(n.value)) {
+        n.setAttribute("class", "red");
+        return false;
+    } else {
+        n.classList.remove("red");
+        return true;
+    }
+}
+
+function verificaSexo() {
+    var s = document.getElementById("sexof");
+    var r = document.getElementById("sexom");
+    var radios = document.getElementsByName("sexo");
+    if (radios[0].checked == true) {
+        s.removeAttribute("class","red");
+        r.removeAttribute("class","red");
+        return true;
+    } else if (radios[1].checked == true) {
+        s.removeAttribute("class","red");
+        r.removeAttribute("class","red");
+        return true;
+    } else {
+        r.setAttribute("class", "red");
+        s.setAttribute("class", "red");
+        return false;
+    }
+
+}
+
+
+function verificaTel() {
+    var xtel = "";
+    var t = document.getElementById("tel").value;
+    var te = document.getElementById("tel");
+
+    if (t.length == 1) {
+        document.getElementById("tel").value = "(" + t;
+    } else if (t.length == 3) {
+        document.getElementById("tel").value = t + ")";
+    } else if (t.length == 9) {
+        document.getElementById("tel").value = t + "-";
+    }
+
+    for (var i = 0; i < t.length; i++) {
+        if (t.charAt(i) != "(" && t.charAt(i) != ")" && t.charAt(i) != "-") {
+            xtel += t.charAt(i);
+        }
+    }
+    if (isNaN(xtel) || xtel.length <= 10 || t == "") {
+        te.setAttribute("class", "red");
+        return false;
+    } else {
+        te.classList.remove("red");
+        return true;
+    }
+}
---
+++ routes/index.js
@@ -0,0 +1,109 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Note = require('../models/note');
+const router = express.Router();
+
+/* GET login page. */
+router.get('/', (req, res, next) => {
+  res.render('login', { title: 'Login' });
+});
+/* GET home page. */
+router.get('/home', (req, res, next) => {
+  res.render('home', { title: 'Home' });
+});
+/* GET cadastroFunc page. */
+router.get('/cadastroFunc', (req, res, next) => {
+  res.render('cadastroFunc', { title: 'Cadastro de Funcionários' });
+});
+/* GET cadastroPeca page. */
+router.get('/cadastroPeca', (req, res, next) => {
+  res.render('cadastroPeca', { title: 'Cadastro de Peças' });
+});
+/* GET cadastroFunc page. */
+router.get('/listFunc', (req, res, next) => {
+  res.render('listFunc', { title: 'Listagem de Funcionários' });
+});
+/* GET cadastroPeca page. */
+router.get('/listarPeca', (req, res, next) => {
+  res.render('listarPeca', { title: 'Listagem de Peças' });
+});
+
+
+
+/* EXEMPLO DO PROFESSOR */
+/* GET lista de notas. */
+router.get('/list', async (req, res, next) => {
+  // mongoose permite usar async/await, então a função callback (que estamos) precisa ser definida como async para poder dizer qual comando deve ser 'awaitado'
+  try {
+    let result = await Note.find().exec();
+    res.render('findAll', { title: 'Mongo', notes: result });
+  } catch (e) {
+    console.error(e);
+  }
+});
+/* GET create new note form. */
+router.get('/create', function (req, res, next) {
+  res.render('create', { title: 'Mongo' });
+});
+/* POST salva anotação e volta para lista */
+router.post('/', async (req, res, next) => {
+  // busca os dados da anotação do form
+  let titulo = req.body.inputTitulo;
+  let conteudo = req.body.inputConteudo;
+  // cria o objeto e insere no banco. nota conterá o objeto inserido
+  let nota = await Note.create({
+    title: titulo,
+    content: conteudo
+  });
+  res.redirect(`/${nota.id}`);
+});
+/* GET search note form. */
+router.get('/find', function (req, res, next) {
+  res.render('findOneForm', { title: 'Mongo' });
+});
+/* GETs para busca por título ou número da anotação. */
+router.get('/ft', async (req, res, next) => {
+  let result = await Note.findOne({ title: req.query.inputTitulo });
+  if (result)
+    res.render('findOne', { title: 'Mongo', note: result });
+  else
+    res.render('notFound', { titulo: req.query.titulo });
+});
+/* GETs para busca por título ou número da anotação. */
+router.get('/fn', async (req, res, next) => {
+  // o _id criado automaticamente pelo Mongo não corresponde a um "número". Seria necessário criar um objeto para poder comparar o id.
+  res.render('notFound', { numero: req.query.numero });
+});
+/* GET para mostrar uma anotação (e opções) */
+router.get('/:id', async function (req, res, next) {
+  try {
+    let result = await Note.findById(req.params.id);
+    if (result)
+      res.render('findOne', { title: 'Mongo', note: result });
+    else
+      res.render('notFound', { numero: req.query.numero });
+  } catch(error) {
+      console.log(error);
+      res.send("Problemas foram encontrados");
+  }
+});
+/* **DELETE** apaga uma anotação e volta para lista */
+router.post('/:id/del', async (req, res, next) => {
+  await Note.findByIdAndRemove(req.params.id);
+  res.redirect('/list');
+});
+/* GET altera uma anotação e volta para ela mesma */
+router.get('/:id/edit', async (req, res, next) => {
+  let noteToChange = await Note.findById(req.params.id);
+  res.render('editOneForm', { title: 'Mongo', note: noteToChange });
+});
+/* **PUT** altera uma anotação e volta para ela mesma */
+router.post('/:id/edit', async (req, res, next) => {
+  let noteToChange = await Note.findById(req.params.id);
+  noteToChange.title = req.body.inputTitulo;
+  noteToChange.content = req.body.inputConteudo;
+  await Note.findByIdAndUpdate(noteToChange.id, noteToChange, { new: true });
+  res.redirect(`/${req.params.id}`);
+});
+
+module.exports = router;
